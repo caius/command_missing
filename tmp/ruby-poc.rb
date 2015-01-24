e2cead49762c699c8c5425a4b8fa047e1767f879
Caius Durling <dev@caius.name>
2015-01-24 01:32:59 +0000

    Make ruby POC work

--- tmp/ruby-poc.rb
+++ tmp/ruby-poc.rb
@@ -1,17 +1,23 @@
-ARGV = %w(bre wup) # !> already initialized constant ARGV
-ENV["PATH"] = "/Users/caius/go/bin:/Users/caius/bin:/Users/caius/.cabal/bin:/usr/local/sbin:/Users/caius/.gem/ruby/2.1.5/bin:/Users/caius/.rubies/ruby-2.1.5/lib/ruby/gems/2.1.0/bin:/Users/caius/.rubies/ruby-2.1.5/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin"
+#!/usr/bin/env ruby
 
 def paths
   @paths ||= ENV["PATH"].split(":")
 end
 
-def cmd?(cmd)
-  paths.any? { |path| File.exist? File.join(path, cmd) }
+def cmd_path(cmd)
+  paths.map { |path| potential = File.join(path, cmd); potential if File.exist?(potential) }.compact.first
 end
 
 def candidates(array)
-  (1..array[0].size).map { |x| array[0][0, x] } | \
+  array[0] ||= ""
+  array[1] ||= ""
+  array[0].size.downto(1).map { |x| array[0][0, x] } | \
   (1..array[1].size).map { |x| array[0] + array[1][0, x] }
 end
 
-candidates(ARGV).find { |cmd| cmd?(cmd) } || exit(0)
+if found_it = candidates(ARGV).map { |cmd| cmd_path(cmd) }.compact.first
+  puts found_it
+  exit(0)
+else
+  exit(1)
+end
